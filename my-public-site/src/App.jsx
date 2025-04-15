871e7e090423fd95aa2d5055a6fb3fb2a95691f3
medhaitam <medhaitam37@gmail.com>
2025-04-15 08:26:14 -0700

    App.jsx

--- my-public-site/src/App.jsx
+++ my-public-site/src/App.jsx
@@ -1,23 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
+import History from './pages/History'
+import ThemeToggle from './components/ThemeToggle' // Nouveau composant
 import './index.css'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className={darkMode ? 'dark' : 'light'}>
+      <BrowserRouter>
+        <Navbar />
+        <ThemeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
 export default App
-
